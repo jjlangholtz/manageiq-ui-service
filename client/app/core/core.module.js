4c7dd3a9c055380c944de8fd08ad9ca7f71508a5
Josh Langholtz <jjlangholtz@gmail.com>
2017-01-17 15:33:22 -0500

    Initial stab at vendor dependency graph

--- client/app/core/core.module.js
+++ client/app/core/core.module.js
@@ -1,28 +1,30 @@
-(function() {
-  'use strict';
+import 'angular-animate';
+import 'angular-messages';
+import 'angular-sanitize';
+import 'angular-base64';
+import 'angular-ui-router';
 
-  angular.module('app.core', [
-    // Angular modules
-    'ngAnimate',
-    'ngSanitize',
-    'ngMessages',
+angular.module('app.core', [
+  // Angular modules
+  'ngAnimate',
+  'ngMessages',
+  'ngSanitize',
 
-    // Blocks modules
-    'blocks.exception',
-    'blocks.logger',
-    'blocks.router',
-    'blocks.multi-transclude',
-    'blocks.pub-sub',
-    'blocks.bind-attrs',
-    'blocks.directive-options',
-    'blocks.recursion',
+  // Blocks modules
+  'blocks.exception',
+  'blocks.logger',
+  'blocks.router',
+  'blocks.multi-transclude',
+  'blocks.pub-sub',
+  'blocks.bind-attrs',
+  'blocks.directive-options',
+  'blocks.recursion',
 
-    'app.skin',
-    'app.resources',
-    'app.services',
+  'app.skin',
+  'app.resources',
+  'app.services',
 
-    // Third party modules
-    'ui.router',
-    'base64',
-  ]);
-})();
+  // Third party modules
+  'ui.router',
+  'base64',
+]);
